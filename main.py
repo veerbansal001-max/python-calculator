14d1921e1c56643777b31b8b63b0eae006e9a6b0
veerbansal001-max <veerbansal001@icloud.com>
2026-06-27 13:52:22 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# python-calculator 
+a= int(input("Inter Your First Number:"))
+b= int(input("Inter Your Second Number: "))
+c= input("select a operator (+,-,*,/,**,//) : ")
+if C =="+":
+   print(a + b)
+elif c ==" ":
+   print (a - b)
+elif c =="*":
+   print (a *b)
+elif c =="/":
+   print (a /b)
+elif C == "**":
+   print (a**b)
+elif c == "//".
+   print (a // b)
+else:
+   print ("Error")
